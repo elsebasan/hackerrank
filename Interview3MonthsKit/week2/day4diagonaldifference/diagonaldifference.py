eb893d6a5d8fc9c54dae4499a1085cee34f945df
Jose Sebastian Sanchez <jose.sanchez@imm.gub.uy>
2021-11-02 16:30:17 -0300

    Changes to be committed: 	new file:   day4diagonaldifference/diagonaldifference.py

---
+++ Interview3MonthsKit/week2/day4diagonaldifference/diagonaldifference.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    lenght = len(arr)
+    principal = 0
+    secondary = 0
+    for i in range(lenght):
+        principal = principal + arr[i][i]
+        secondary = secondary + arr[i][lenght-1-i]
+    return abs(principal - secondary)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
