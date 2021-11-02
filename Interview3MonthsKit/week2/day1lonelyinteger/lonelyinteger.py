7024f46bf54cec17597bf2a259defee1d2ad37c2
Jose Sebastian Sanchez <jose.sanchez@imm.gub.uy>
2021-11-02 13:49:10 -0300

    new file:   week2/day1lonelyinteger/lonelyinteger.py

---
+++ Interview3MonthsKit/week2/day1lonelyinteger/lonelyinteger.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    lenght = len(a)
+    result = {}
+    for elem in a:
+        if elem in result:
+            result.pop(elem, None)
+        else:
+            result[elem]=elem   
+    return list(result.keys())[0]
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
