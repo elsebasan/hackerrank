0cab4a287bb632d60cd57203c20e1a0510879a4e
Jose Sebastian Sanchez <jose.sanchez@imm.gub.uy>
2021-11-03 15:40:11 -0300

    Changes to be committed: 	new file:   day5countingsort/coutingsort.py

---
+++ Interview3MonthsKit/week2/day5countingsort/coutingsort.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    return_array = []  
+    for i in range(100):
+        return_array.append(0)
+    for elem in arr:
+        return_array[elem] = return_array[elem] + 1
+    return return_array
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
